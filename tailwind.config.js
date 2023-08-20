f761ead9f8aed8dd77086f3732ed876ab5d7b083
HariBalaji96 <harianandbalaji@gmail.com>
2023-08-20 10:49:31 +0530

    Notes

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      bluish: "#05386b",
+      grayish: "#379683",
+      sand: "#5cdb95",
+      whites: "#edf5e1",
+    },
+  },
+  plugins: [],
+};

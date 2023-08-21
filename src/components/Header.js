7ed27d4977e6d2d41123ccdbea54d58ec4a55c7d
HariBalaji96 <harianandbalaji@gmail.com>
2023-08-21 15:22:27 +0530

    Header Completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { PlusIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-center bg-sand text-bluish text-5xl p-5 font-semibold">
-        QUIZ
+    <div className="flex items-center justify-center fixed w-full">
+      <h1 className="bg-sand text-bluish text-5xl p-5 font-semibold w-full">
+        Notes
       </h1>
+      <button title="Add Notes" className="absolute top-6 right-20">
+        <PlusIcon className="text-bluish w-10 h-10 hover:bg-bluish hover:text-sand rounded-full p-1" />
+      </button>
+      <button title="Search" className="absolute top-6 right-6">
+        <MagnifyingGlassIcon className="text-bluish w-10 h-10 hover:bg-bluish hover:text-sand rounded-full p-1" />
+      </button>
     </div>
   );
 };

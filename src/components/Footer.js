052f18e5c9b1c2d792b8b1c1877914d00f29deaf
HariBalaji96 <harianandbalaji@gmail.com>
2023-08-23 11:54:59 +0530

    Footer Copyrights

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import React from "react";
 const Footer = ({ length }) => {
   return (
     <div className="text-bluish bg-sand border-t border-grayish w-full h-[10vh]">
-      <h1 className="p-5 text-center font-semibold">Total Notes: {length}</h1>
+      <h1 className="p-2 text-center font-semibold">Total Notes: {length}</h1>
+      <p className="text-center">&copy; Crafted By Hari Anand</p>
     </div>
   );
 };
